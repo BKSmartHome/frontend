300218642f3fd1cb96a918ca7d6ca23e342b5a4b
paradox2604 <66421827+paradox2604@users.noreply.github.com>
2023-05-05 01:15:20 +0700

    Update SettingPane.tsx

--- screens/app/components/SettingPane.tsx
+++ screens/app/components/SettingPane.tsx
@@ -1,93 +1,256 @@
-import { Switch } from "@material-tailwind/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Input,
+  Switch,
+  Tab,
+  TabPanel,
+  Tabs,
+  TabsBody,
+  TabsHeader,
+  Typography,
+} from "@material-tailwind/react";
 import { cx } from "@utils/tools";
 import { connect, MqttClient } from "mqtt";
 import { useEffect, useMemo, useState } from "react";
-import {
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-  Input,
-} from "@material-tailwind/react";
-
 
 import styles from "./styles.module.scss";
 
 export const SettingPane: IComponent = () => {
-
-  const changePassword = useMemo(() => {
-    return (
-      <div>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-          <div className="mb-4 flex flex-col gap-6">
-            <Input type="password" label="Old Password" nonce={undefined} onResize={undefined} onResizeCapture={undefined} />
-            <Input type="password" label="New Password" nonce={undefined} onResize={undefined} onResizeCapture={undefined} />
-            <Input type="password" size="lg" label="Type New Password" nonce={undefined} onResize={undefined} onResizeCapture={undefined} />
-          </div>
-        </form>
-      </div>
-    );
-  }, []);
-
-  const alert = useMemo(() => {
-    return (
-      <div className="bg-white flex justify-center">
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-          <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Name" nonce={undefined} onResize={undefined} onResizeCapture={undefined} />
-            <Input size="lg" label="Email" nonce={undefined} onResize={undefined} onResizeCapture={undefined} />
-            
-          </div>
-        </form>
-      </div>
-    );
-  }, []);
-
-  const tabSetting = useMemo(() => {
-    const data = [
-      {
-        label: "Change Password",
-        value: "Change Password",
-        desc: changePassword, 
-      },
-      {
-        label: "Alert",
-        value: "Alert",
-        desc: alert,
-      },
-    ];
-
-    return (
-      <Tabs value="html">
-          <TabsHeader nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
-            {data.map(({ label, value }) => (
-              <Tab key={value} value={value} nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
-                {label}
-              </Tab>
-            ))}
-          </TabsHeader>
-          <TabsBody nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
-            {data.map(({ value, desc }) => (
-              <TabPanel key={value} value={value}>
-                {desc}
-              </TabPanel>
-            ))}
-          </TabsBody>
-      </Tabs>
-    );
-  }, []);
+  const data = [
+    {
+      label: "Mật khẩu",
+      value: "password",
+      children: (
+        <div className="flex justify-center items-center">
+          <Card className="w-1/2 ">
+            <CardHeader
+              nonce={undefined}
+              onResize={undefined}
+              onResizeCapture={undefined}
+              floated={false}
+              shadow={false}
+              className="h-12 items-center grid border-b"
+            >
+              <Typography
+                variant="h5"
+                className="flex justify-center items-center"
+              >
+                Đổi mật khẩu
+              </Typography>
+            </CardHeader>
+            <CardBody
+              nonce={undefined}
+              onResize={undefined}
+              onResizeCapture={undefined}
+            >
+              <Typography variant="h6"> Mật khẩu cũ </Typography>
+              <Input
+                nonce={undefined}
+                onResize={undefined}
+                onResizeCapture={undefined}
+                type="text"
+                placeholder="Nhập mật khẩu cũ"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                className={
+                  "rounded-md !border-t-purple-gray-200 focus:!border-t-purple-500 "
+                }
+              />
+              <Typography variant="h6" className="pt-3">
+                {" "}
+                Mật khẩu mới{" "}
+              </Typography>
+              <Input
+                type="text"
+                placeholder="Nhập mật khẩu mới"
+                className="rounded-md !border-t-purple-gray-200 focus:!border-t-purple-500"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                containerProps={{
+                  className: "min-w-0",
+                }}
+              />
+              <Typography variant="h6" className="pt-3">
+                {" "}
+                Xác nhận mật khẩu{" "}
+              </Typography>
+              <Input
+                type="text"
+                placeholder="Nhập lại mật khẩu mới"
+                className="rounded-md !border-t-purple-gray-200 focus:!border-t-purple-500"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                containerProps={{
+                  className: "min-w-0",
+                }}
+              />
+            </CardBody>
+            <CardFooter className="flex items-center justify-center">
+              <Button
+                className="py-2 px-4 bg-purple-500 text-white font-semibold rounded-md hover:bg-purple-700"
+                size="lg"
+              >
+                Xác nhận
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      ),
+    },
+    {
+      label: "Thông báo",
+      value: "notification",
+      children: (
+        <div className="flex justify-center items-center">
+          <Card className="w-1/2">
+            <CardHeader
+              nonce={undefined}
+              onResize={undefined}
+              onResizeCapture={undefined}
+              floated={false}
+              shadow={false}
+              className="h-12 items-center grid border-b"
+            >
+              <Typography
+                variant="h5"
+                className="flex justify-center items-center"
+              >
+                Thiết lập thông báo
+              </Typography>
+            </CardHeader>
+            <CardBody
+              nonce={undefined}
+              onResize={undefined}
+              onResizeCapture={undefined}
+            >
+              <Typography variant="h6"> Tên người nhận </Typography>
+              <Input
+                nonce={undefined}
+                onResize={undefined}
+                onResizeCapture={undefined}
+                type="text"
+                placeholder="Nhập tên người nhận"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                className={
+                  "rounded-md !border-t-purple-gray-200 focus:!border-t-purple-500 "
+                }
+              />
+              <Typography variant="h6" className="pt-3">
+                {" "}
+                Email người nhận{" "}
+              </Typography>
+              <Input
+                type="mail"
+                placeholder="Nhập email người nhận"
+                className="rounded-md !border-t-purple-gray-200 focus:!border-t-purple-500"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                containerProps={{
+                  className: "min-w-0",
+                }}
+              />
+              <Typography variant="h6" className="pt-3">
+                {" "}
+                Loại thông báo{" "}
+              </Typography>
+              <div className="space-y-3 pt-2 flex flex-col">
+                <Switch
+                  nonce={undefined}
+                  onResize={undefined}
+                  onResizeCapture={undefined}
+                  id="temperature"
+                  label="Thông báo về cảm biến nhiệt độ"
+                  color="purple"
+                  defaultChecked={false}
+                />
+                <Switch
+                  nonce={undefined}
+                  onResize={undefined}
+                  onResizeCapture={undefined}
+                  id="light"
+                  label="Thông báo về cảm biến ánh sáng"
+                  color="purple"
+                  defaultChecked={false}
+                />
+                <Switch
+                  nonce={undefined}
+                  onResize={undefined}
+                  onResizeCapture={undefined}
+                  id="soilmoisture"
+                  label="Thông báo về cảm biến độ ẩm đất"
+                  color="purple"
+                  defaultChecked={false}
+                />
+                <Switch
+                  nonce={undefined}
+                  onResize={undefined}
+                  onResizeCapture={undefined}
+                  id="smoke"
+                  label="Thông báo về cảm biến khói"
+                  color="purple"
+                  defaultChecked={false}
+                />
+                <Switch
+                  nonce={undefined}
+                  onResize={undefined}
+                  onResizeCapture={undefined}
+                  id="infrared"
+                  label="Thông báo về cảm biến hồng ngoại"
+                  color="purple"
+                  defaultChecked={false}
+                />
+              </div>
+            </CardBody>
+            <CardFooter className="flex items-center justify-center">
+              <Button
+                className="py-2 px-4 bg-purple-500 text-white font-semibold rounded-md hover:bg-purple-700"
+                size="lg"
+              >
+                Xác nhận
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      ),
+    },
+  ];
 
   return (
-    <div className="p-8">
+    <main className="space-y-1">
       <div className="text-white text-4xl justify-between font-semibold flex gap-2 mb-8">
         <h1>Smart Home</h1>
         <h1>10:06 AM, Mar 2 2023</h1>
       </div>
-
-      <div className="min-h-[60vh] grid grid-rows-2 gap-8 ">
-        {tabSetting}
-      </div>
-    </div>
+      <Tabs>
+        <TabsHeader>
+          {data.map(({ label, value }) => (
+            <Tab
+              key={value}
+              value={value}
+              className={cx("p-3 font-bold rounded-inherit")}
+            >
+              {label}
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody className="mt-4">
+          {data.map(({ value, children }) => (
+            <TabPanel key={value} value={value} className=" rounded-lg">
+              {children}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+    </main>
   );
 };
